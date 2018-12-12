de2bd8fc75e306740b00d2419cd89dfb93b99f43
Tom Brodhurst-Hill <developer@barefeetware.com>
2018-12-13 10:52:22 +1100

    Initial Commit

---
+++ Social/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Social
+//
+//  Created by Tom Brodhurst-Hill on 13/12/18.
+//  Copyright © 2018 BareFeetWare. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
