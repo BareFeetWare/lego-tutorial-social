fe56e9ea31379617a4594287dc9d4455b159eba1
Tom Brodhurst-Hill <developer@barefeetware.com>
2019-03-02 20:16:43 +1100

    Products: custom cell and auto adjusting heights

---
+++ Social/ProductTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ProductTableViewCell.swift
+//  Social
+//
+//  Created by Tom Brodhurst-Hill on 30/12/18.
+//  Copyright © 2018 BareFeetWare. All rights reserved.
+//
+
+import UIKit
+import BFWControls
+
+@IBDesignable class ProductTableViewCell: NibTableViewCell {
+    
+}
