3f0fdb3bc7111643bade4fa6991b1582b188881f
Tom Brodhurst-Hill <developer@barefeetware.com>
2019-03-02 20:18:07 +1100

    Chat: Added cells with left and right message bubbles

---
+++ Social/LeftChatTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  LeftChatTableViewCell.swift
+//  Social
+//
+//  Created by Tom Brodhurst-Hill on 2/1/19.
+//  Copyright © 2019 BareFeetWare. All rights reserved.
+//
+
+import UIKit
+import BFWControls
+
+@IBDesignable class LeftChatTableViewCell: NibTableViewCell {
+    
+}
---
+++ Social/RightChatTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  RightChatTableViewCell.swift
+//  Social
+//
+//  Created by Tom Brodhurst-Hill on 31/12/18.
+//  Copyright © 2018 BareFeetWare. All rights reserved.
+//
+
+import UIKit
+import BFWControls
+
+@IBDesignable class RightChatTableViewCell: NibTableViewCell {
+    
+}
