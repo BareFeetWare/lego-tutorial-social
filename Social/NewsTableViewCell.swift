2d86e012d2638d33470d632f5278fdcd8c859ddc
Tom Brodhurst-Hill <developer@barefeetware.com>
2019-03-02 20:00:09 +1100

    Created NewsTableViewCell with custom layout.

---
+++ Social/NewsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NewsTableViewCell.swift
+//  Social
+//
+//  Created by Tom Brodhurst-Hill on 18/12/18.
+//  Copyright © 2018 BareFeetWare. All rights reserved.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
